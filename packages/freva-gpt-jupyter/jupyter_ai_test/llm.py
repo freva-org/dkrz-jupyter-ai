217cacd788a49b3426e515aaeef545791121c906
Felix Oertel <oertel@dkrz.de>
2024-12-02 17:19:43 +0100

    feat: stream code instead of returning it as a block, add debug option to use an example conversation instead of calling chatbot api, include CodeOutput variant in output of stream, implement call method for use in %%ai magic commands

--- packages/freva-gpt-jupyter/jupyter_ai_test/llm.py
+++ packages/freva-gpt-jupyter/jupyter_ai_test/llm.py
@@ -1,9 +1,12 @@
 import json
+import os
+from pathlib import Path
 from typing import Any, Dict, List, Optional, Iterator
 
 from PIL import Image
 import base64
 import io
+import time
 
 from langchain_core.pydantic_v1 import (
     BaseModel,
@@ -17,9 +20,10 @@ from langchain_core.utils import (
 )
 
 from langchain_core.callbacks.manager import CallbackManagerForLLMRun
+from langchain_core.language_models.chat_models import generate_from_stream
 from langchain_core.language_models.llms import LLM
-from langchain_core.messages import ChatMessageChunk, ChatMessage, AIMessageChunk
-from langchain_core.outputs import ChatGenerationChunk
+from langchain_core.messages import ChatMessageChunk, ChatMessage, AIMessage, AIMessageChunk
+from langchain_core.outputs import ChatGenerationChunk, ChatGeneration, ChatResult
 
 from ._client import Client
 from ._types import Message
@@ -33,6 +37,7 @@ class FrevaChat(LLM):
     _client: Client = Field(default=None)
     stop: str = Field(default="Generation complete")
     thread_id: str = Field(default=None)
+    debug: bool = False
 
     @property
     def _llm_type(self) -> str:
@@ -55,11 +60,13 @@ class FrevaChat(LLM):
             message=AIMessageChunk(content=message.content)
             )
     
-    def _translate_to_chat_message(
+    def _translate_to_chat_generation(
             self,
             message: Message
-    ) -> ChatMessage:
-        return ChatMessage(content=message.content, role=message.variant)
+    ) -> ChatGeneration:
+        return ChatGeneration(
+            message=AIMessage(content=message.content)
+        )
     
     def _call(
         self,
@@ -69,8 +76,9 @@ class FrevaChat(LLM):
         **kwargs: Any,
     ) -> ChatMessage:
         
-        r = self._client.request(method="GET", url="/streamresponse", stream=True, params={"input":prompt, "auth_key":self.auth_key})
-        return "this is a test!"
+        stream = self._stream(prompt, stop, run_manager, **kwargs)
+        chat_result: ChatResult = generate_from_stream(stream)
+        return chat_result.generations[0].text
         
         
     def _stream(
@@ -84,44 +92,75 @@ class FrevaChat(LLM):
         if stop is None:
             stop=self.stop
 
+        if self.debug:
+            with open(file=f"{Path(__file__).parent}/example_conversation.json") as fo:
+                stream=json.load(fo)
+        else:
+            stream=self._client.request(method="GET", url="/streamresponse", stream=True, params={"input":prompt, "auth_key":self.auth_key.get_secret_value(), "thread_id":self.thread_id or None})
         first_part=True
         code_started=False
         code_content=""
-        for part in self._client.request(method="GET", url="/streamresponse", stream=True, params={"input":prompt, "auth_key":self.auth_key.get_secret_value(), "thread_id":self.thread_id or None}):
+        for part in stream:
             content = part["content"]
-            print("content:", content)
-            if part["variant"]=="ServerHint":
+            variant = part["variant"]
+            if variant=="ServerHint":
                 if stop in content:
                     break
                 if first_part:
                     if not self.thread_id: self.thread_id = json.loads(content)["thread_id"]
                     first_part=False
                     continue
-                elif code_started:
-                    finished_code=json.loads(code_content)
-                    print("finished code:", finished_code)
-                    message=Message(
-                        variant="Code", 
-                        content="\n\n```python\n" + finished_code["code"] + "\n```\n\n"
-                    )
-                    code_started=False
-                    code_content=""
                 else:
                     continue
-            elif part["variant"]=="Code":
+            elif variant=="Code":
                 code_content+=content[0]
-                code_started=True
-                continue
-            elif part["variant"]=="Image":
-                image_string=part["content"]
-                image_bytes=io.BytesIO(base64.b64decode(image_string))
-                img=Image.open(image_bytes)
-                print("saving image and trying to display it in the chat!")
-                img.save("output.png")
-                message=Message(variant="Image", content='\n\n ![alt text](output.png "Title")\n\n')
-            elif part["variant"]=="Assistant":
+                if code_content=='{"code":"':
+                    code_started=True
+                    code_content="\n```python\n"
+                    message=Message(
+                            variant="Code",
+                            content=code_content
+                        )
+                    code_content=""
+                elif code_started:
+                    if code_content[-2:]=='\\n':
+                        code_content=code_content.replace('\\n', '\n')
+                        message=Message(
+                            variant="Code",
+                            content=code_content
+                        )
+                        code_content=""
+                    elif content[0]=='"}':
+                        code_content="\n```\n\n"
+                        code_started=False
+                        message=Message(
+                            variant="Code",
+                            content=code_content
+                        )
+                        code_content=""
+                    elif '\\' not in code_content:
+                        message= Message(
+                            variant="Code",
+                            content=code_content
+                        )
+                        code_content=""
+                    else:
+                        continue
+                else:
+                    continue
+            elif variant=="CodeOutput":
+                message=Message(
+                        variant="CodeOutput",
+                        content="\n```\n" +content[0]+"\n```\n"
+                )
+            elif variant=="Image":
+                base64_string=content
+                message=Message(
+                    variant="Image", 
+                    content=f'\n\n ![alt text](data:image/png;base64,{base64_string})\n\n'
+                )
+            elif variant=="Assistant":
                 message=Message(**part)
             else:
                 continue
-            print("message:", message)
             yield self._translate_to_chat_generation_chunk(message)
