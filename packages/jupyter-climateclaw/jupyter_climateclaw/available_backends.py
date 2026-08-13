cfcfb0353bda59c02bbf6fcb141c114dec724947
Felix Oertel <oertel@dkrz.de>
2026-08-13 14:55:09 +0200

    update available chatbots

--- packages/jupyter-climateclaw/jupyter_climateclaw/available_backends.py
+++ packages/jupyter-climateclaw/jupyter_climateclaw/available_backends.py
@@ -1,11 +1,4 @@
 # available backends (top is default)
 available_backends = [
-    ## GPT models ##
-    "gpt-4.1",
-    "gpt-4.1-mini",
-    "ministral-3:14b",
-    # "o1-mini",
-    ## Ollama models ##
-    # "llama3.2:3B",
-    "qwen2.5:3b",
+'gpt-4.1', 'gpt-5.6-luna', 'gpt-4.1-mini', 'gemma4:31b', 'gemma4:26b', 'ministral-3:8b', 'mistral-small3.2:24b', 'mistral-medium-3.5:128b', 'qwen3.6:35b', 'local',
 ]
